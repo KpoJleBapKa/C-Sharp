0caff0567641231854f6cc1807a270574f047987
KpoJleBapKa <ars05@ukr.net>
2022-09-15 10:22:35 +0300

    init

--- лб1/Program.cs
+++ лб1/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-
+// Обчислення опору електричного ланцюга, що складається із двох паралельно з'єднаних резисторів
+// R - опір. 1/R = 1/R_1 + 1/R_2
 namespace лб1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while(true)
+            {
+                double R, R_1, R_2;
+                bool flag = false;
+                do
+                {
+                    Console.WriteLine("Введіть значення першого резистора: ");
+                    flag = double.TryParse(Console.ReadLine(), out R_1);
+                } while (R_1 <= 0 || !flag);
+                flag = false;
+                do
+                {
+                    Console.WriteLine("Введіть значення другого резистора: ");
+                    flag = double.TryParse(Console.ReadLine(), out R_2);
+                } while (R_2 <= 0 || !flag);
+            
+                R = (1 / R_1) + (1 / R_2);
+                Console.WriteLine("Відповідь: " + R);
+                Console.ReadLine();
+            }
         }
     }
 }
