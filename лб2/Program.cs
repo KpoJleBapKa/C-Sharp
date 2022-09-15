29310429aca32f065bf35c8306061ab7c61f5909
KpoJleBapKa <ars05@ukr.net>
2022-09-15 10:25:12 +0300

    init

---
+++ лб2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace лб2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
