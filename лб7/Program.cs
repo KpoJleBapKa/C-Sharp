93cd6ee8ce64487d80ca5bea12a408644a0ef881
KpoJleBapKa <ars05@ukr.net>
2022-10-04 16:33:10 +0300

    init

---
+++ лб7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace лб7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
