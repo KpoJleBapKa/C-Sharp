8b29521941cc4ca28602321cee9b5c0cd73c5449
KpoJleBapKa <ars05@ukr.net>
2022-10-05 20:29:42 +0300

    init

--- лб6/Program.cs
+++ лб6/Program.cs
@@ -8,59 +8,6 @@ namespace лб6
 {
     class Program
     {
-        static int IndexOf(string phrase, int startIndex, char[] separators, out char currentSeparator)
-        {
-            currentSeparator = ' ';
-            for (var i = startIndex; i < phrase.Length; i++)
-            {
-                for (var j = 0; j < separators.Length; j++)
-                {
-                    if (phrase[i] == separators[j])
-                    {
-                        currentSeparator = separators[j];
-                        return i;
-                    }
-                }
-            }
-            return -1;
-        }
-
-        static string WordsReverse(string phrase)
-        {
-            var phraseLength = phrase.Length;
-            var retValue = new char[phraseLength];
-            var wordStartIndex = 0;
-
-            while (wordStartIndex < phraseLength)
-            {
-                var separatorIndex = IndexOf(phrase, wordStartIndex, new[] { ' ', ',', '.' }, out char currentSeparator);
-                if (separatorIndex == -1)
-                {
-                    separatorIndex = phrase.Length;
-                }
-                else
-                {
-                    retValue[separatorIndex] = currentSeparator;
-                }
-
-                var currentIndex = wordStartIndex;
-                for (var i = separatorIndex - 1; i >= wordStartIndex; i--)
-                {
-                    retValue[currentIndex] = phrase[i];
-                    currentIndex++;
-                }
-
-                wordStartIndex = separatorIndex + 1;
-            }
-
-            return new string(retValue);
-        }
-
-        static void Main(string[] args)
-        {
-            var text = Console.ReadLine();
-            Console.WriteLine(WordsReverse(text));
-            Console.ReadKey();
-        }
+        
     }
 }
