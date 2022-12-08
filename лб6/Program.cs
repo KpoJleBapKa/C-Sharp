d9164246ecc7e6b8647a1dd03ba711ee63f2cd08
KpoJleBapKa <ars05@ukr.net>
2022-12-08 14:26:05 +0200

    init

--- лб6/Program.cs
+++ лб6/Program.cs
@@ -8,6 +8,19 @@ namespace лб6
 {
     class Program
     {
-        
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.WriteLine("Enter some text: ");
+            string str = Console.ReadLine(); // Вводим строку
+            string reversed = ""; // здесь будет наша перевернутая строка
+            for (i = str.Length - 1; i >= 0; i--)
+            {
+                reversed += str[i]; // переворачиваем строку
+            }
+            Console.WriteLine("The result: ");
+            Console.WriteLine(reversed);
+            Console.ReadKey();
+        }
     }
 }
