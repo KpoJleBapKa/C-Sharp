ad42a910191145e20b805fb63fb67f8c3edd1eb6
KpoJleBapKa <ars05@ukr.net>
2022-12-21 23:28:22 +0200

    init

---
+++ лб8.1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace лб8._1
+{
+    class Program
+    {
+        public abstract class Phone
+        {
+            public abstract void MakeOutgoingCall(string phoneNumber);
+            public abstract void ReceiveIncomingCall(string phoneNumber);
+        }
+
+        public class DialPhone : Phone
+        {
+            public override void MakeOutgoingCall(string phoneNumber)
+            {
+                // Реалізація для здійснення вихідного дзвінка за допомогою телефонного номера
+            }
+
+            public override void ReceiveIncomingCall(string phoneNumber)
+            {
+                // Реалізація прийому вхідного дзвінка за допомогою телефонного номера
+            }
+        }
+
+        public class ButtonPhone : Phone
+        {
+            public override void MakeOutgoingCall(string phoneNumber)
+            {
+                // Реалізація для здійснення вихідного дзвінка за допомогою кнопкового телефону
+            }
+
+            public override void ReceiveIncomingCall(string phoneNumber)
+            {
+                // Реалізація прийому вхідного дзвінка за допомогою кнопкового телефону
+            }
+
+            public void DisplayCallerId(string phoneNumber)
+            {
+                // Реалізація відображення номера телефону абонента на екрані
+            }
+        }
+
+        public class MobilePhone : Phone
+        {
+            public override void MakeOutgoingCall(string phoneNumber)
+            {
+                // Реалізація для здійснення вихідного дзвінка за допомогою мобільного телефону
+            }
+
+            public override void ReceiveIncomingCall(string phoneNumber)
+            {
+                // Реалізація прийому вхідного дзвінка за допомогою мобільного телефону
+            }
+
+            public void SendSms(string phoneNumber, string message)
+            {
+                // Реалізація для відправки SMS за допомогою мобільного телефону
+            }
+
+            public void ReceiveSms(string phoneNumber, string message)
+            {
+                // Реалізація для отримання SMS на мобільний телефон
+            }
+        }
+
+        public class SmartPhone : MobilePhone
+        {
+            public void CreatePhoto()
+            {
+                // Реалізація для створення фото за допомогою смартфона
+            }
+
+            public void CreateVideo()
+            {
+                // Реалізація для створення відео за допомогою смартфона
+            }
+        }
+    }
+}
