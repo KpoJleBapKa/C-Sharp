562e81de4f7745b3cb31a119d809cbaa89b663a7
KpoJleBapKa <113384296+KpoJleBapKa@users.noreply.github.com>
2022-12-25 04:16:54 +0200

    Update Program.cs

--- лб9.2/Program.cs
+++ лб9.2/Program.cs
@@ -8,10 +8,8 @@ namespace лб9._2
     {
         public class Picture
         {
-            // The collection of shapes in the picture
             private List<Shape> shapes;
 
-            // Constructors
             public Picture()
             {
                 shapes = new List<Shape>();
@@ -21,35 +19,26 @@ namespace лб9._2
             {
                 shapes = new List<Shape>(length);
             }
-
-            // Property to get the number of elements in the collection
             public int Count => shapes.Count;
 
-            // Method to add a new shape to the collection
             public void AddShape(Shape shape)
             {
                 shapes.Add(shape);
             }
-
-            // Method to delete a shape from the collection by name
             public void DeleteShapeByName(string name)
             {
                 shapes.RemoveAll(s => s.Name == name);
             }
 
-            // Method to delete a shape from the collection by type
             public void DeleteShapeByType(Type type)
             {
                 shapes.RemoveAll(s => s.GetType() == type);
             }
-
-            // Method to delete a shape from the collection if it exceeds a given area
             public void DeleteShapeByArea(double maxArea)
             {
                 shapes.RemoveAll(s => s.Area > maxArea);
             }
 
-            // Indexer to get an element from the collection by index
             public Shape this[int index]
             {
                 get { return shapes[index]; }
